115f60c98e3d641c449de41a24c400736c6a4cf4
Dudko888 <sashavdudko@icloud.com>
2023-01-02 22:57:48 +0200

    Some changes

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,6 @@ public class Main {
 
         double longitude = 25.3424400;
 
-     //   double latitude = 50.7593200;
-
-     //   double longitude = 25.3424400;
-
         System.out.println(latitude + "°");
         System.out.println(longitude + "°");
 
